e52b17cd41882bbc777dd6957ac5496edc764059
Liam Seper <lseper2@huskers.unl.edu>
2023-11-15 20:09:33 -0800

    add interval util

--- src/util.rs
+++ src/util.rs
@@ -275,6 +275,26 @@ impl Display for Vec3 {
     }
 }
 
+pub struct Interval {
+    pub min: f32,
+    pub max: f32
+}
+
+impl Interval {
+    pub fn contains(&self, x: f32) -> bool {
+        self.min <= x && x <= self.max
+    }
+
+    pub fn size(&self) -> f32 {
+        self.max - self.min
+    }
+
+    pub fn expand(&self, delta: f32) -> Interval {
+        let padding = delta / 2.0;
+        Interval { min: self.min - padding, max: self.max + padding}
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
